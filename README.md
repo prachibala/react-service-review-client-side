0a5b5120e04953899e83d4d39885cf53b78d9d65
Prachi Bala <prachibala330@gmail.com>
2022-11-11 22:42:37 +0600

    v-1.7

--- README.md
+++ README.md
@@ -1,14 +1,43 @@
-Language Used
-Frontend:
-React
-tailwind css
+# SnackBox
 
-Backend:
-Nodejs
-Express
+## Site Links
 
-Database:
-MongoDB
+#### Live Site Link
 
-Authentication:
-Firebase Auth
+```
+ https://snackbox-4656d.web.app/
+```
+
+#### Private Repo Client Side Link
+
+```
+https://github.com/Porgramming-Hero-web-course/b6a11-service-review-client-side-prachibala
+```
+
+#### Private Repo Server Side Link
+
+````
+https://github.com/Porgramming-Hero-web-course/b6a11-service-review-server-side-prachibala```
+
+````
+
+## What We have used in this Project
+
+1. React (Frontend)
+2. NodeJS/Express (Backend)
+3. Firebase (Authentication)
+4. JWT (Backend Route Authorization)
+5. Tailwind, DaisyUi & Mamba UI(Frontend Design)
+6. MongoDB (Database)
+
+## Project Speciality
+
+1. Single Page Application
+2. Authentication can be done with email/password and google signin.
+
+## Project Description
+
+-   SnackBox is made for Food Lovers.
+-   It is an online snack menu deliver and review system.
+-   Users can search and order their most liked menus.
+-   Users can also add their opinion(review) with rating.
--- src/Pages/Review/MyReviews.js
+++ src/Pages/Review/MyReviews.js
@@ -1,104 +1,154 @@
 import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../Context/Context";
+import { toast } from "react-toastify";
+import Loading from "../../Loader/Loading";
 
 const MyReveiws = () => {
-	const { user } = useContext(AuthContext);
-	const [reviews, setReviews] = useState([]);
-	useEffect(() => {
-		if (user) {
-			fetch(
-				`${process.env.REACT_APP_server_url}/reviews-by-user/${user.uid}`,
-				{
-					headers: {
-						"Content-Type": "application/json",
-						authorization: `Bearer ${localStorage.getItem(
-							"token"
-						)}`,
-					},
-				}
-			)
-				.then((res) => res.json())
-				.then((data) => {
-					setReviews(data);
-					// setLoading(false);
-				})
-				.catch((err) => console.log(err));
-		}
-	}, [user]);
+    const { user } = useContext(AuthContext);
+    const [reviews, setReviews] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-	return (
-		<div className="container p-2 mx-auto sm:p-4 dark:text-gray-800 mb-10">
-			<h2 className="mb-4 text-2xl font-semibold leading-tight">
-				My Reviews
-			</h2>
-			<div className="overflow-x-auto">
-				<table className="min-w-full text-xs">
-					<colgroup>
-						<col />
-						<col />
-						<col />
-						<col />
-						<col />
-					</colgroup>
-					<thead className="dark:bg-lime-100">
-						<tr className="text-left">
-							<th className="p-3">Menu</th>
-							<th className="p-3">Review</th>
-							<th className="p-3">Rating</th>
-							<th className="p-3">Delete</th>
-							<th className="p-3"></th>
-						</tr>
-					</thead>
-					<tbody>
-						{reviews.map((review) => (
-							<tr
-								className="border-b border-opacity-20 dark:border-gray-700 "
-								key={review._id}
-							>
-								<td className="p-3">
-									<p>{review.menu.menuName}</p>
-								</td>
-								<td className="p-3">
-									<p>{review.reviewText}</p>
-								</td>
-								<td className="p-3">
-									<p>{review.rating}</p>
-								</td>
+    const notify = () => {
+        toast.success("Review Has Been Deleted Successfully", {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        });
+    };
 
-								<td className="p-3">
-									<button className="btn btn-ghost dark:bg-error sm:btn-sm md:btn-md lg:btn-md">
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											viewBox="0 0 24 24"
-											fill="currentColor"
-											className="w-4 h-4 mr-1"
-										>
-											<path
-												fillRule="evenodd"
-												d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
-												clipRule="evenodd"
-											/>
-										</svg>
-										<span>Delete</span>{" "}
-									</button>
-								</td>
+    useEffect(() => {
+        if (user) {
+            fetch(
+                `${process.env.REACT_APP_server_url}/reviews-by-user/${user.uid}`,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        authorization: `Bearer ${localStorage.getItem(
+                            "token"
+                        )}`,
+                    },
+                }
+            )
+                .then((res) => res.json())
+                .then((data) => {
+                    setReviews(data);
+                    setLoading(false);
+                })
+                .catch((err) => console.log(err));
+        }
+    }, [user]);
 
-								<td className="p-3 text-right">
-									<Link
-										className="btn btn-ghost dark:bg-lime-400 sm:btn-sm md:btn-md lg:btn-md"
-										to={`/menus/${review.menu.menuId}`}
-									>
-										View Menu
-									</Link>
-								</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
-			</div>
-		</div>
-	);
+    const handleDelete = (id) => {
+        fetch(`${process.env.REACT_APP_server_url}/delete-review/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                const newReviews = reviews.filter((review) => {
+                    return review._id !== id;
+                });
+                setReviews(newReviews);
+                notify();
+            })
+            .catch((err) => console.log(err));
+    };
+
+    return (
+        <div className="container p-2 mx-auto sm:p-4 dark:text-gray-800 mb-10">
+            <h2 className="mb-4 text-2xl font-semibold leading-tight">
+                My Reviews
+            </h2>
+            <div className="overflow-x-auto">
+                {loading ? (
+                    <Loading />
+                ) : reviews?.length > 0 ? (
+                    <table className="min-w-full text-xs">
+                        <colgroup>
+                            <col />
+                            <col />
+                            <col />
+                            <col />
+                            <col />
+                        </colgroup>
+                        <thead className="dark:bg-lime-100">
+                            <tr className="text-left">
+                                <th className="p-3">Menu</th>
+                                <th className="p-3">Review</th>
+                                <th className="p-3">Rating</th>
+                                <th className="p-3">Delete</th>
+                                <th className="p-3"></th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            {reviews.map((review) => (
+                                <tr
+                                    className="border-b border-opacity-20 dark:border-gray-700 "
+                                    key={review._id}
+                                >
+                                    <td className="p-3">
+                                        <p>{review.menu.menuName}</p>
+                                    </td>
+                                    <td className="p-3">
+                                        <p>{review.reviewText}</p>
+                                    </td>
+                                    <td className="p-3">
+                                        <p>{review.rating}</p>
+                                    </td>
+
+                                    <td className="p-3">
+                                        <button
+                                            className="btn btn-ghost dark:bg-error sm:btn-sm md:btn-md lg:btn-md"
+                                            onClick={() =>
+                                                handleDelete(review._id)
+                                            }
+                                        >
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 24 24"
+                                                fill="currentColor"
+                                                className="w-4 h-4 mr-1"
+                                            >
+                                                <path
+                                                    fillRule="evenodd"
+                                                    d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
+                                                    clipRule="evenodd"
+                                                />
+                                            </svg>
+                                            <span>Delete</span>
+                                        </button>
+                                    </td>
+
+                                    <td className="p-3 text-right">
+                                        <Link
+                                            className="btn btn-ghost dark:bg-lime-400 sm:btn-sm md:btn-md lg:btn-md"
+                                            to={`/menus/${review.menu.menuId}`}
+                                        >
+                                            View Menu
+                                        </Link>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                ) : (
+                    <h4 className="text-lg text-center text-lime-700">
+                        You didn't review any Menu Yet.
+                    </h4>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default MyReveiws;
